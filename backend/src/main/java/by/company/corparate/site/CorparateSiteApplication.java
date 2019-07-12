4e2e7693696b01ddadd347d12f090fc021767ec6
Andrew <andreiblinets@gmail.com>
2019-07-12 18:35:08 +0300

    create and init project

---
+++ backend/src/main/java/by/company/corparate/site/CorparateSiteApplication.java
@@ -0,0 +1,13 @@
+package by.company.corparate.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorparateSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorparateSiteApplication.class, args);
+	}
+
+}

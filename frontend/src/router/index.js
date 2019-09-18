7862a071f60fc06c023d69b942536576a43f08d6
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2019-09-18 20:44:34 +0300

    init

---
+++ frontend/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from '@/views/Home.vue'
+
+Vue.use(Router)
+
+export const constantRoutes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/About.vue')
+  }
+]
+
+const createRouter = () => new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+export default router

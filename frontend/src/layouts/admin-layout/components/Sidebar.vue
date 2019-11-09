01235ead047866c21e03d73b5bbf541220319925
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2019-11-09 13:07:17 +0300

    add admin dashboard layout

--- frontend/src/layouts/admin-layout/components/Sidebar.vue
+++ frontend/src/layouts/admin-layout/components/Sidebar.vue
@@ -1,16 +1,23 @@
 <template>
-  <el-menu>
+  <el-menu
+    background-color="#304156"
+    text-color="#bfcbd9"
+  >
     <el-menu-item index="1">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-news"></i>
       <span>Новоси</span>
     </el-menu-item>
     <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span>Navigator Two</span>
+      <i class="el-icon-tickets"></i>
+      <span>Проекты</span>
     </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <span>Navigator Three</span>
+    <el-menu-item index="3">
+      <i class="el-icon-s-cooperation"></i>
+      <span>Партнёры</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-s-custom"></i>
+      <span>Вакансии</span>
     </el-menu-item>
   </el-menu>
 </template>
--- frontend/src/layouts/admin-layout/index.vue
+++ frontend/src/layouts/admin-layout/index.vue
@@ -1,31 +1,49 @@
 <template>
-  <el-container class="admin-wrapper">
-    <el-aside>
-      <sidebar class="sidebar-container"/>
-    </el-aside>
-    <el-container>
-      <el-header></el-header>
-      <el-main>
-        
-      </el-main>
-    </el-container>
-  </el-container>
+  <div class="admin-wrapper">
+    <sidebar class="sidebar-container"/>
+    <navbar class="navbar-container"/>
+    <div class="main-container">
+      <app-main />
+    </div>
+  </div>
 </template>
 
 <script>
+import AppMain from './components/AppMain'
+import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 
 export default {
   name: 'AdminLayout',
   components: {
+    AppMain,
+    Navbar,
     Sidebar
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-.admin-wrapper
-  width: 100vw
-  height: 100vh
-  position: relative
+<style scoped>
+.admin-wrapper {
+  display: grid;
+  grid-template-columns: 210px calc(100vw - 210px);
+  grid-template-rows: 50px calc(100vh - 50px);
+  grid-template-areas:
+    "sidebar navbar"
+    "sidebar main";
+}
+
+.navbar-container {
+  grid-area: navbar;
+}
+
+.sidebar-container {
+  grid-area: sidebar;
+}
+
+.main-container {
+  grid-area: main;
+  padding: 20px;
+  background-color: #f0f2f5;
+}
 </style>

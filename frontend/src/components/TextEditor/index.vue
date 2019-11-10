eabd869d27044a7a0022a92cb82499dbf2721390
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2019-11-10 23:51:06 +0300

    init text editor

---
+++ frontend/src/components/TextEditor/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-editor-container">
+    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+      <button :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+        Bold
+      </button>
+    </editor-menu-bar>
+    <editor-content :editor="editor" />
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import { Bold } from 'tiptap-extensions'
+
+export default {
+  name: 'TextEditor',
+  components: {
+    EditorMenuBar,
+    EditorContent
+  },
+  data() {
+    return {
+      editor: new Editor({
+        content: '<p>Контент</p>',
+        extensions: [
+          new Bold(),
+        ],
+      })
+    };
+  },
+  beforeDestroy() {
+    this.editor.destroy();
+  }
+}
+</script>

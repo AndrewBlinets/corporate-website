3d1b9a859f9cff03afefb427a5225935bd7f7ee8
Andrew Blinets <andreiblinets@gmail.com>
2019-11-11 10:52:18 +0300

    remove

--- DaoModul/src/main/java/com/ipps/corporate/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package com.ipps.corporate.entity;
-
-public enum Role {
-    SUPERADMINISTRATOR, ADMINISTRATOR, WORKER, USER
-}
--- DaoModul/src/main/java/com/ipps/corporate/entity/Sector.java
+++
@@ -1,6 +0,0 @@
-package com.ipps.corporate.entity;
-
-import java.io.Serializable;
-
-public class Sector extends AbstractEntity implements Serializable {
-}

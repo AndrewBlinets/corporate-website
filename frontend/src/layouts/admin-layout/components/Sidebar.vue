5359a498351df2acd17e83a97aa2b299d578cfde
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-11-18 10:29:36 +0300

    add link google drive

--- frontend/src/layouts/admin-layout/components/Sidebar.vue
+++ frontend/src/layouts/admin-layout/components/Sidebar.vue
@@ -21,6 +21,10 @@
       <i class="el-icon-s-custom"></i>
       <span>Вакансии</span>
     </el-menu-item>
+    <el-menu-item index="5">
+      <i class="fab fa-google-drive icon"></i>
+      <el-link class="link-menu" target="_blank" href="https://docs.google.com/presentation/d/1WNnmVUKoe8WZ2rTh1xuwkjghVYVDRh_StFHiSVSkPr0/edit?usp=sharing">Google Drive</el-link>
+    </el-menu-item>    
   </el-menu>
 </template>
 
@@ -29,3 +33,16 @@ export default {
   name: 'Sidebar'
 }
 </script>
+
+<style lang="stylus" scoped>
+.icon
+  margin-right: 5px
+  width: 24px
+  text-align: center
+  font-size: 18px
+  vertical-align: middle
+  color: #909399
+
+.link-menu
+  color: rgb(191, 203, 217)
+</style>

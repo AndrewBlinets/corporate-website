453366948796db2fc6e479e3c894eb727a2fa762
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-11-19 16:22:51 +0300

    add layout

---
+++ frontend/account-ipps/src/layout/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout">
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+};
+</script>
+
+<style lang="stylus" scoped>
+main
+  max-width: 460px
+  margin: 0 auto
+  margin-top: 80px
+</style>

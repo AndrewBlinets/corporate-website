0267bf2d9a91d664d866c1e1fd15ee4aa4aeb5c7
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-11-29 11:18:05 +0300

    added request util

---
+++ frontend/admin-ipps/src/utils/request.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import store from '@/store';
+import { Message } from 'element-ui';
+
+const service = axios.create({
+  baseURL: 'http://www.ipps.by:5454',
+  timeout: 5000
+});
+
+service.interceptors.request.use(
+  config => {
+
+    if (store.getters.token) {
+      config.headers['Authorization'] = `Bearer ${store.getters.token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+service.interceptors.response.use(
+  response => {
+    const { data } = response;
+    
+    return data;
+  },
+  error => {
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 5000
+    });
+    return Promise.reject(error);
+  }
+);
+
+export default service;

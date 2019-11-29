06c931b21f04b5025fa15fdb3cc7cbe7284d14b5
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-11-29 11:13:00 +0300

    added router app

--- frontend/admin-ipps/src/router/index.js
+++ frontend/admin-ipps/src/router/index.js
@@ -1,15 +1,65 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import Layout from '@/layout';
+
 Vue.use(VueRouter);
 
-const routes = [
+export const constantRoutes = [
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    meta: {
+      title: 'Dashboard'
+    },
+    children: [
+      {
+        path: 'dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard'),        
+      },
+    ]
+  },
+];
+
+export const asyncRoutes = [
+  {
+    path: '/news',
+    component: Layout,
+    meta: {
+      title: 'Новости',
+      roles: ['super-admin']
+    },
+    children: [
+      {
+        path: '',
+        name: 'news',
+        component: () => import('@/views/news')
+      }
+    ]
+  },
+  {
+    path: '/projects',
+    component: Layout,
+    meta: {
+      title: 'Проекты',
+      roles: ['super-admin', 'super-editor']
+    },
+    children: [
+      {
+        path: '',
+        name: 'projects',
+        component: () => import('@/views/news')
+      }
+    ]
+  }
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: constantRoutes
 });
 
 export default router;

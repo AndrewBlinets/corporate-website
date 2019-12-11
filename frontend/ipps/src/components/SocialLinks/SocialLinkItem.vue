309d22dc34d17891f96aaa529d7748e0be21d72b
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-12-11 11:24:34 +0300

    added social links component

---
+++ frontend/ipps/src/components/SocialLinks/SocialLinkItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <a :href="url" class="social-link">
+    <font-awesome-icon :icon="fortAwesomeIcon" />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'SocialLinkItem',
+  props: {
+    url: {
+      type: String,
+      default: ''
+    },
+    fortAwesomeIcon: {
+      type: Object,
+      default: function() {
+        return {}
+      }
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.social-link
+  margin-right: 15px
+  color: #fff
+  border: 1px solid #fff
+  width: 40px
+  height: 40px
+  line-height: 40px
+  border-radius: 50%
+  text-align: center
+  display: inline-block
+  font-size: 14px
+  transition: .5s
+  &:hover
+    color: #1461ff
+    background: #fff
+</style>
---
+++ frontend/ipps/src/components/SocialLinks/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="social-links__container">
+    <social-link-item 
+      v-for="socialLink in socialLinks"
+      v-bind="socialLink"
+      :key="socialLink.id"
+    />
+  </div>
+</template>
+
+<script>
+import { faFacebookF, faVk, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import SocialLinkItem from './SocialLinkItem';
+
+export default {
+  name: 'SocialLinks',
+  components: {
+    SocialLinkItem
+  },
+  data() {
+    return {
+      socialLinks: [
+        {
+          id: 1,
+          url: '',
+          fortAwesomeIcon: faFacebookF
+        },
+        {
+          id: 2,
+          url: '',
+          fortAwesomeIcon: faVk
+        },
+        { 
+          id: 3,
+          url: '',
+          fortAwesomeIcon: faTwitter
+        },
+        {
+          id: 4,
+          url: '',
+          fortAwesomeIcon: faYoutube
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.social-links__container
+  display: flex  
+</style>

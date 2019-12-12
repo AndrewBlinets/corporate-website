93fb9a2bde9167eed32a0d58b1bcf4ebd8a29b94
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-12-12 11:09:41 +0300

    added format date filter

--- frontend/ipps/src/main.js
+++ frontend/ipps/src/main.js
@@ -5,11 +5,13 @@ import locale from 'element-ui/lib/locale/lang/ru-RU';
 import 'element-ui/lib/theme-chalk/index.css';
 
 import App from './App.vue';
+
 import router from '@/router';
 import store from '@/store';
+import * as filters from '@/filters';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCommentAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCommentAlt, faUserTie, faBriefcase, faClock } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faVk, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
@@ -17,13 +19,17 @@ import '@/styles/index.styl';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
-library.add(faCommentAlt, faFacebookF, faVk, faTwitter, faYoutube);
+library.add(faCommentAlt, faFacebookF, faVk, faTwitter, faYoutube, faUserTie, faBriefcase, faClock);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI, { locale });
+
+Object.keys(filters).forEach((key) => {
+  Vue.filter(key, filters[key]);
+});
 
 new Vue({
   router,

87e34fa093f92d3ffba7ad758f3b3c929e0aa262
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-12-13 16:02:26 +0300

    added component section

---
+++ frontend/ipps/src/components/Section/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <section>
+      <div class="app-container">
+        <div class="section-header">
+          <h2>{{ name }}</h2>
+        </div>
+        <div class="section-body">
+          <component :is="$options.components[componentName]" />
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+import About from './components/About';
+import Contact from './components/Contact';
+import News from './components/News';
+
+export default {
+  name: 'Section',
+  components: {
+    About,
+    Contact,
+    News
+  },
+  data() {
+    return {
+      name: 'Test Section',
+      componentName: 'News'
+    };
+  }
+};
+</script>

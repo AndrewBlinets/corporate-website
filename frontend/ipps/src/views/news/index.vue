46f9111ac8468adfffa4793eca0e8d640a112dd4
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-12-13 16:05:54 +0300

    added content in page projects and news

--- frontend/ipps/src/views/news/index.vue
+++ frontend/ipps/src/views/news/index.vue
@@ -1,9 +1,98 @@
 <template>
-  <div>List news</div>
+  <div>
+    <div class="header-page">
+      <div class="app-container">
+        <div class="title">
+          <h1>Новости</h1>
+        </div>
+      </div>
+    </div>
+    <div class="body-page">
+      <div class="app-container">
+
+        <div class="row mb-3">
+          <div class="col-xl-4 col-lg-12 col-md-6 px-2">
+            <news-card-item />
+          </div>
+          <div class="col-xl-8 px-2">
+            <news-card-list />
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-3 px-2">
+            <news-card-item />
+          </div>
+          <div class="col-3 px-2">
+            <news-card-item />
+          </div>
+          <div class="col-3 px-2">
+            <news-card-item />
+          </div>
+          <div class="col-3 px-2">
+            <news-card-item />
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            <news-card-list />
+          </div>
+          <div class="col-6">
+            <div class="row">
+              <div class="col-12">
+                <news-card-item />
+              </div>
+              <div class="col-12">
+                <news-card-item />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import NewsCardList from '@/components/CardNews/NewsCardList';
+import NewsCardItem from '@/components/CardNews/NewsCardItem';
+
 export default {
-  name: 'News'
+  name: 'News',
+  components: {
+    NewsCardList,
+    NewsCardItem
+  }
 };
 </script>
+
+<style lang="stylus" scoped>
+.header-page
+  position: relative
+  background-image: url(../../images/news.jpg)
+  background-size: cover
+  background-position: center 60%
+  z-index: 1
+  .title
+    display: flex
+    height: 300px
+    align-items: flex-end
+    h1
+      color: #fff
+      margin-bottom: 4rem
+  &:before
+    content: ""
+    position: absolute
+    right: 0
+    bottom: 0
+    width: 100%
+    height: 100%
+    z-index: -1;
+    background: -webkit-linear-gradient(-45deg,rgba(88, 195, 255,.7) 0,rgba(0, 77, 230,.7) 40%,rgba(0, 59, 177,.7) 100%)
+
+.body-page
+  margin-top: 100px
+  margin-bottom: 100px
+</style>
--- frontend/ipps/src/views/projects/index.vue
+++ frontend/ipps/src/views/projects/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Projects</div>
+  <div>
+    <div class="header-page">
+      <div class="app-container">
+        <div class="title">
+          <h1>Проекты</h1>
+        </div>
+      </div>
+    </div>
+    <div class="body-page">
+      <div class="app-container">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +19,32 @@ export default {
   name: 'Projects'
 };
 </script>
+
+<style lang="stylus" scoped>
+.header-page
+  position: relative
+  background-image: url(../../images/projects.jpg)
+  background-size: cover
+  background-position: center 60%
+  z-index: 1
+  .title
+    display: flex
+    height: 300px
+    align-items: flex-end
+    h1
+      color: #fff
+      margin-bottom: 4rem
+  &:before
+    content: ""
+    position: absolute
+    right: 0
+    bottom: 0
+    width: 100%
+    height: 100%
+    z-index: -1;
+    background: -webkit-linear-gradient(-45deg,rgba(88, 195, 255,.7) 0,rgba(0, 77, 230,.7) 40%,rgba(0, 59, 177,.7) 100%)
+
+.body-page
+  margin-top: 100px
+  margin-bottom: 100px
+</style>

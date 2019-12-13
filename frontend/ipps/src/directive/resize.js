364b2de5831f655dc18582e1fd9328429274cb9f
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-12-13 15:47:35 +0300

    added directive resize

---
+++ frontend/ipps/src/directive/resize.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+
+export const Resize = {
+  inserted(el, binding) {
+    const f = (evt) => {
+      if (binding.value(evt, el)) {
+        window.removeEventListener('resize', f);
+      }
+    };
+    window.addEventListener('resize', f);
+  }
+};
+
+Vue.directive('resize', Resize);

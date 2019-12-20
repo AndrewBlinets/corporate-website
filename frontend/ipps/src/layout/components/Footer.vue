60605f3db9194d0a2486655025ce9eea80c4885b
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-12-20 16:01:35 +0300

    added content footer

--- frontend/ipps/src/layout/components/Footer.vue
+++ frontend/ipps/src/layout/components/Footer.vue
@@ -1,8 +1,30 @@
 <template>
   <footer>
     <div class="app-container">
-      <div class="footer-container">
-        <social-links />
+      <div class="row my-5">
+        <div class="col-4">
+          <div class="links-item">
+            <social-links />
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="links-item">
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="links-item">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-container">
+      <div class="app-container">
+        <div class="content">
+          <div class="logo">
+            <img src="../../images/logoIPPS.png" alt="ИППС">
+          </div>
+          <p>© НИРУП «Институт прикладных программных систем»</p>
+        </div>
       </div>
     </div>
   </footer>
@@ -23,6 +45,24 @@ export default {
 footer
   margin: auto 0 0
   background: rgb(0,59,177)
+  .links-item
+    display: flex
+    align-items: flex-start
+    width: 100%
+    height: 100%
+  .links-item
+    border-right: 1px solid #ffffff50
   .footer-container
-    height: 600px
+    padding: 30px 0
+    border-top: 1px solid #ffffff50
+    .content
+      display: flex
+      align-items: center
+      .logo
+        margin-right: 15px
+        img
+          width: 50px
+          height: 50px
+      p
+        color: #fff
 </style>

ee39b0be1af6f0a33b805fbecf986bbc82ffe869
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-12-24 10:51:30 +0300

    import heder page component in news page

--- frontend/ipps/src/views/news/index.vue
+++ frontend/ipps/src/views/news/index.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-    <div class="header-page">
-      <div class="app-container">
-        <div class="title">
-          <h1>Новости</h1>
-        </div>
-      </div>
-    </div>
+    <header-page
+      :name="namePage"
+      :imageId="imagePageId"
+    />
     <div class="body-page">
       <div class="app-container">
 
-        <div class="row mb-3">
+        <div class="row mb-3 mx-n2">
           <div class="col-lg-4 col-md-12 px-2 mb-lg-0 mb-3">
             <news-card-item />
           </div>
@@ -53,42 +50,27 @@
 </template>
 
 <script>
+import HeaderPage from '@/components/HeaderPage';
 import NewsCardList from '@/components/CardNews/NewsCardList';
 import NewsCardItem from '@/components/CardNews/NewsCardItem';
 
 export default {
   name: 'News',
   components: {
+    HeaderPage,
     NewsCardList,
     NewsCardItem
+  },
+  data() {
+    return {
+      namePage: 'Новости',
+      imagePageId: 16
+    };
   }
 };
 </script>
 
 <style lang="stylus" scoped>
-.header-page
-  position: relative
-  background-image: url(../../images/news.jpg)
-  background-size: cover
-  background-position: center 60%
-  z-index: 1
-  .title
-    display: flex
-    height: 300px
-    align-items: flex-end
-    h1
-      color: #fff
-      margin-bottom: 4rem
-  &:before
-    content: ""
-    position: absolute
-    right: 0
-    bottom: 0
-    width: 100%
-    height: 100%
-    z-index: -1;
-    background: -webkit-linear-gradient(-45deg,rgba(88, 195, 255,.7) 0,rgba(0, 77, 230,.7) 40%,rgba(0, 59, 177,.7) 100%)
-
 .body-page
   margin-top: 100px
   margin-bottom: 100px

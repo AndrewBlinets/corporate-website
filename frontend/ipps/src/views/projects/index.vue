4d8c9d1d91f790b9a593ee0ce4a43af05a65d3a3
Alexei Zubarik <alesha.zubarik@yandex.ru>
2019-12-24 10:52:35 +0300

    added style projects page

--- frontend/ipps/src/views/projects/index.vue
+++ frontend/ipps/src/views/projects/index.vue
@@ -1,49 +1,65 @@
 <template>
   <div>
-    <div class="header-page">
-      <div class="app-container">
-        <div class="title">
-          <h1>Проекты</h1>
-        </div>
-      </div>
-    </div>
+    <header-page
+      :name="namePage"
+      :imageId="imagePageId"
+    />
     <div class="body-page">
       <div class="app-container">
+        <div class="row mx-n2 mb-5">
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+          <div class="col-lg-4 col-sm-6 px-2 mb-3">
+            <project-card-item />
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import HeaderPage from '@/components/HeaderPage';
+import ProjectCardItem from '@/components/ProjectCard/ProjectCardItem';
+
 export default {
-  name: 'Projects'
+  name: 'Projects',
+  components: {
+    HeaderPage,
+    ProjectCardItem
+  },
+  data() {
+    return {
+      namePage: 'Проекты',
+      imagePageId: 17
+    };
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
-.header-page
-  position: relative
-  background-image: url(../../images/projects.jpg)
-  background-size: cover
-  background-position: center 60%
-  z-index: 1
-  .title
-    display: flex
-    height: 300px
-    align-items: flex-end
-    h1
-      color: #fff
-      margin-bottom: 4rem
-  &:before
-    content: ""
-    position: absolute
-    right: 0
-    bottom: 0
-    width: 100%
-    height: 100%
-    z-index: -1;
-    background: -webkit-linear-gradient(-45deg,rgba(88, 195, 255,.7) 0,rgba(0, 77, 230,.7) 40%,rgba(0, 59, 177,.7) 100%)
-
 .body-page
   margin-top: 100px
   margin-bottom: 100px

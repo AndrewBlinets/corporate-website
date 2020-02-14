8ab9c07f8b9bf10ef960822bb20feaf0b483c213
Alexei Zubarik <alesha.zubarik@yandex.ru>
2020-02-14 15:47:25 +0300

    added upload file component

---
+++ frontend/admin-ipps/src/components/UploadFile/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <el-upload
+      action="#"
+      list-type="picture-card"
+      :file-list="fileList"
+      :auto-upload="false"
+      :limit="1"
+    >
+      <i slot="default" class="el-icon-plus" />
+      <div class="file" slot="file" slot-scope="{file}">
+        <img
+          class="el-upload-list__item-thumbnail"
+          :src="file.url"
+          alt=""
+        >
+        <span class="el-upload-list__item-actions">
+          <span
+            class="el-upload-list__item-preview"
+            @click="handlePictureCardPreview(file)"
+          >
+            <i class="el-icon-zoom-in" />
+          </span>
+          <span
+            v-if="!disabled"
+            class="el-upload-list__item-delete"
+            @click="handleDownload(file)"
+          >
+            <i class="el-icon-download" />
+          </span>
+          <span
+            v-if="!disabled"
+            class="el-upload-list__item-delete"
+            @click="handleRemove(file)"
+          >
+            <i class="el-icon-delete" />
+          </span>
+        </span>
+      </div>
+    </el-upload>
+    <el-dialog :visible.sync="dialogVisible">
+      <img width="100%" :src="dialogImageUrl" alt="">
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UploadFile',
+  data() {
+    return {
+      fileList: [],
+      dialogImageUrl: '',
+      dialogVisible: false,
+      disabled: false,
+    };
+  },
+  methods: {
+    handleRemove(file) {
+      // eslint-disable-next-line no-console
+      console.log(file);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    handleDownload(file) {
+      // eslint-disable-next-line no-console
+      console.log(file);
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.file
+  width: 100%
+  height: 100%
+  .el-upload-list__item-thumbnail
+    object-fit: cover
+    object-position: center center
+</style>

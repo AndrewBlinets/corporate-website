ed48031eac01d717c0a630d689d3b59429da2f68
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-02-17 00:09:01 +0300

    edited component news

--- frontend/admin-ipps/src/views/news/index.vue
+++ frontend/admin-ipps/src/views/news/index.vue
@@ -1,29 +1,24 @@
 <template>
-  <div class="news-container">
-    <div class="name">
+  <div class="page-container">
+    <div class="title">
       <h1>Новости</h1>
     </div>
     <div class="button-container">
       <el-button
         type="success"
         icon="el-icon-plus"
-        @click="toCreateArticle"
+        @click="$router.push({ name: 'create-news' })"
       >Создать</el-button>
     </div>
-    <div class="table-container">
+    <div class="table-container mb-1">
       <el-table
         ref="multipleTable"
-        :data="news"
+        :data="newsList"
         :row-class-name="tableRowClassName"
         border
       >
         <el-table-column
           type="index"
-        />
-        <el-table-column
-          prop="id"
-          label="Id"
-          width="50"
         />
         <el-table-column
           prop="shortTitle"
@@ -52,64 +47,95 @@
           width="230"
         >
           <template slot-scope="scope">
-            <el-tooltip effect="dark" content="Просмотреть" placement="top">
-              <el-button size="medium" icon="el-icon-view" />
-            </el-tooltip>
-            <el-tooltip effect="dark" content="Редактировать" placement="top">
-              <el-button size="medium" icon="el-icon-edit" />
-            </el-tooltip>
-            <el-tooltip effect="dark" content="Удалить" placement="top">
-              <el-button
-                @click.native.prevent="deleteRow(scope, tableData)"
-                size="medium"
-                icon="el-icon-delete"
-                type="danger"
-              />
-            </el-tooltip>
+            <!-- <el-button size="medium" icon="el-icon-view" /> -->
+            <el-button
+              @click="$router.push({ name: 'news-id', params: { id: scope.row.id } })"
+              size="medium"
+              icon="el-icon-edit"
+            />
+            <el-button
+              @click="deleteRow(scope.row.id)"
+              size="medium"
+              icon="el-icon-delete"
+              type="danger"
+            />
           </template>
         </el-table-column>
       </el-table>
-    </div>    
-  </div>  
+    </div>
+
+    <el-pagination
+      @size-change="tableSizeChange"
+      @current-change="tableCurrentChange"
+      :current-page.sync="page"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import store from '@/store';
+import { mapState, mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'News',
   computed: {
-    ...mapGetters([
-      'news'
-    ])
+    ...mapState({
+      size: state => state.news.params.size
+    }),
+    ...mapGetters({
+      newsList: 'news/newsListData',
+      total: 'news/newsListTotal'
+    }),
+    page: {
+      get: function() {
+        return store.state.news.params.page + 1;
+      },
+      set: function(value) {
+        store.commit('news/ADD_PARAMS', { page: value - 1});
+      }
+    }
   },
-  created() {
-    this.$store.dispatch('news/getNews');
+  beforeRouteEnter(to, from, next) {
+    store.dispatch('news/getNewsList').then(() => {
+      next();
+    });
   },
   methods: {
-    toCreateArticle() {
-      this.$router.push({ path: 'create'});
+    ...mapActions({
+      deleteNews: 'news/deleteNews'
+    }),
+    tableSizeChange(value) {
+      store.dispatch('news/getNewsList', { size: value });
+    },
+    tableCurrentChange(value) {
+      store.dispatch('news/getNewsList', { page: value - 1 });
     },
     tableRowClassName({ row }) {
       if (row.status === 'Опубликовано') {
         return 'success-row';
-      } else if (row.status === 'черновик') {
+      } else if (row.status === 'Черновик') {
         return 'warning-row';
       } else if (row.status === 'Удалён') {
         return 'error-row';
       }
       return '';
     },
-    deleteRow(index, rows) {
+    deleteRow(id) {
       this.$confirm('Вы точно хотите удалить?', {
           confirmButtonText: 'Подтвердить',
           cancelButtonText: 'Отменить',
           type: 'warning'
         }).then(() => {
-          rows.splice(index, 1);
-          this.$message({
-            type: 'success',
-            message: 'Запись удалина'
+          this.deleteNews(id).then(() => {
+            this.$message({
+              type: 'success',
+              message: 'Запись удалина'
+            });
           });
         }).catch(() => {
           this.$message({
@@ -121,20 +147,3 @@ export default {
   },
 };
 </script>
-
-<style lang="stylus" scoped>
-.news-container
-  display: flex
-  flex-direction: column
-  padding: 20px
-  background-color: #fff
-  .name
-  .button-container
-    margin-bottom: 30px
- 
-.el-table__row
-  .warning-row
-    background: oldlace
-  .success-row
-    background: #f0f9eb
-</style>

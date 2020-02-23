8519d1fbee848c359382e9daa9cb4efda7c97e4d
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-02-23 23:14:29 +0300

    changed view project, added new project card

--- frontend/ipps/src/views/projects/index.vue
+++ frontend/ipps/src/views/projects/index.vue
@@ -1,38 +1,30 @@
 <template>
   <div>
-    <header-page
-      :name="namePage"
-      :imageId="imagePageId"
-    />
+    <header-page :image="imagePageId">
+      <h1>Проекты</h1>
+    </header-page>
     <div class="body-page">
       <div class="app-container">
-        <div class="row mx-n2 mb-5">
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
+        <div class="row mx-md-n4">
+          <div
+            v-for="item in projects"
+            :key="item.id"
+            class="col-lg-4 col-sm-6 px-md-4 mb-5"
+          >
+            <project-card
+              :id="item.id"
+              :title="item.shortTitle"
+              :image="item.mainImage"
+            />
           </div>
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
-          </div>
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
-          </div>
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
-          </div>
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
-          </div>
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
-          </div>
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
-          </div>
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
-          </div>
-          <div class="col-lg-4 col-sm-6 px-2 mb-3">
-            <project-card-item />
+        </div>
+
+        <div class="button-container">
+          <div v-if="!hasProjectsFull" class="button-item">
+            <div
+              class="btn btn-main"
+              @click="loadMore"
+            >Загрузить ещё</div>
           </div>
         </div>
       </div>
@@ -41,26 +33,48 @@
 </template>
 
 <script>
+import store from '@/store';
+import { mapState, mapGetters } from 'vuex';
 import HeaderPage from '@/components/HeaderPage';
-import ProjectCardItem from '@/components/ProjectCard/ProjectCardItem';
+import ProjectCard from '@/components/ProjectCard';
 
 export default {
   name: 'Projects',
   components: {
     HeaderPage,
-    ProjectCardItem
+    ProjectCard
   },
-  data() {
-    return {
-      namePage: 'Проекты',
-      imagePageId: 17
-    };
+  data: () => ({
+    titlePage: 'Проекты',
+    imagePageId: 17
+  }),
+  computed: {
+    ...mapState({
+      projects: state => state.project.projectsList,
+      hasProjectsFull: state => state.project.hasProjectsFull
+    }),
+    ...mapGetters({
+      page: 'project/page'
+    })
+  },
+  beforeRouteEnter (to, from, next) {
+    store.dispatch('project/getProjects', { size: 9, page: 0 }).then(() => {
+      next();
+    });
+  },
+  destroyed() {
+    store.dispatch('project/resetProjects');
+  },
+  methods: {
+    loadMore() {
+      store.dispatch('project/getProjects', { page: this.page + 1 });
+    }
   }
 };
 </script>
 
 <style lang="stylus" scoped>
 .body-page
-  margin-top: 100px
-  margin-bottom: 100px
+  margin-top: 50px
+  margin-bottom: 50px
 </style>

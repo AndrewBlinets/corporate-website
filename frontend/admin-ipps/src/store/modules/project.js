1fd872d76f542a06f7a0be98685cb0e9e19256cc
Alexei Zubarik <alesha.zubarik@yandex.ru>
2020-02-24 15:14:42 +0300

    updated store module project

--- frontend/admin-ipps/src/store/modules/project.js
+++ frontend/admin-ipps/src/store/modules/project.js
@@ -7,58 +7,84 @@ import {
 } from '@/api/project';
 
 const state = {
-  departmentsList: [],
-  department: {}
+  projectsList: {},
+  params: {
+    size: 10,
+    page: 0
+  },
+  project: {}
 };
 
 const mutations = {
   SET_PROJECTS_LIST: (state, list) => {
-    state.departmentsList = list;
+    state.projectsList = list;
   },
-  SET_PROJECT: (state, department) => {
-    state.department = department;
+  ADD_PARAMS: (state, params) => {
+    Object.assign(state.params, params);
+  },
+  REMOVE_PARAMS: (state, name) => {
+    delete state.params[name];
+  },
+  SET_PARAMS_LIST: (state, params) => {
+    const { size, page} = params;
+    state.size = size;
+    state.page = page;
+  },
+  SET_PROJECT: (state, project) => {
+    state.project = project;
   }
 };
 
-const getters = {};
+const getters = {
+  projectsListData: state => state.projectsList.content,
+  projectsListTotal: state => state.projectsList.totalElements
+};
 
 const actions = {
-  getProjectsList({ commit }) {
-    return new Promise((resolve) => {
-      getProjectsList().then(data => {
+  getProjectsList({ state, commit }, params = {}) {
+    commit('ADD_PARAMS', params);
+    return new Promise(resolve => {
+      getProjectsList(state.params).then(data => {
         commit('SET_PROJECTS_LIST', data);
         resolve();
       });
     });
   },
-  getProjectById({ commit }, id) {
-    return new Promise((resolve) => {
+  getProject({ commit }, id) {
+    return new Promise(resolve => {
       getProject(id).then(data => {
         commit('SET_PROJECT', data);
         resolve();
       });
     });
   },
-  createProject() {
+  createProject({ commit }, project) {
+    return new Promise(resolve => {
+      createProject(project).then(data => {
+        commit('SET_PROJECT', data);
+        resolve(data.id);
+      });
+    });
+  },
+  updateProject({ commit }, project) {
     return new Promise((resolve) => {
-      createProject().then(() => {
+      updateProject(project).then(data => {
+        commit('SET_PROJECT', data);
         resolve();
       });
     });
   },
-  updateProject() {
-    return new Promise((resolve) => {
-      updateProject().then(() => {
-        resolve();
-      });
-    });
-  },
-  deleteProjectById(id) {
-    return new Promise((resolve) => {
+  deleteProject({ dispatch }, id) {
+    return new Promise(resolve => {
       deleteProject(id).then(() => {
+        dispatch('getProjectsList');
         resolve();
       });
     });
+  },
+  resetProject({ commit }) {
+    commit('SET_PROJECT', {});
+    return Promise.resolve();
   }
 };
 

5f1e91b3546c97b7a2d0b63dcebfaf6356210e9b
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-11 16:48:27 +0300

    fix eslint warning

--- frontend/ipps/src/api/news.js
+++ frontend/ipps/src/api/news.js
@@ -4,7 +4,7 @@ export function getNewsList(params) {
   return request({
     url: '/news',
     method: 'GET',
-    params
+    params,
   });
 }
 
@@ -12,6 +12,6 @@ export function getNewsById(id, params) {
   return request({
     url: `/news/${id}`,
     method: 'GET',
-    params
+    params,
   });
 }

9a9fc1b2b2e3cd0668bbf22245e5e9d1c0be5f3a
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-16 17:54:52 +0300

    add global component Contact in page

--- frontend/ipps/src/views/home/components/Contacts.vue
+++ frontend/ipps/src/views/home/components/Contacts.vue
@@ -7,49 +7,51 @@
       <div class="section-body">
         <div class="row">
           <div class="col-lg-6 col-12 mb-lg-0 mb-5">
-            <div class="contact-info">
-              <div class="about">
-                <i class="fas fa-map-marker-alt icon"/>
-                <span class="about-title">Адрес</span>
-                <span>: 220013 г. Минск, ул.Беломорская, 18</span>
-              </div>
-              <div class="about">
-                <i class="fas fa-phone-alt icon"></i>
-                <span class="about-title">Телефон</span>
-                <span>: (8-017) 290-07-66</span>
-              </div>
-              <div class="about">
-                <i class="fas fa-envelope icon"></i>
-                <span class="about-title">Email</span>
-                <span>: info@ipps.by</span>
-              </div>
-            </div>
-            <div class="note">
-              <p>Телефон «горячей линии» - (8-017) 290-07-66</p>
-              <div class="list">
-                <div class="header">
-                  <p>По номеру «горячей линии» можно обратиться:</p>
+            <contact>
+              <contact-info-list
+                address="220013 г. Минск, ул.Беломорская, 18"
+                phone="(8-017) 290-07-66"
+                email="info@ipps.by"
+              ></contact-info-list>
+
+              <contact-note>
+                <p>Телефон «горячей линии» - (8-017) 290-07-66</p>
+                <div class="list">
+                  <div class="header">
+                    <p>По номеру «горячей линии» можно обратиться:</p>
+                  </div>
+                  <ul>
+                    <li>
+                      по вопросам справочно-консультационного характера,
+                      связанного с направлениями деятельности организации;
+                    </li>
+                    <li>
+                      по вопросам выявления фактов вымогательства,
+                      взяточничества и других проявлений коррупции со стороны
+                      работников организации;
+                    </li>
+                    <li>
+                      по вопросам записи на личный прием к руководству
+                      организации.
+                    </li>
+                  </ul>
                 </div>
-                <ul>
-                  <li>по вопросам справочно-консультационного характера, связанного с направлениями деятельности организации;</li>
-                  <li>по вопросам выявления фактов вымогательства, взяточничества и других проявлений коррупции со стороны работников организации;</li>
-                  <li>по вопросам записи на личный прием к руководству организации.</li>
-                </ul>
-              </div>
-              <div class="list">
-                <div class="header">
-                  <p>Режим работы горячей линии:</p>
+                <div class="list">
+                  <div class="header">
+                    <p>Режим работы горячей линии:</p>
+                  </div>
+                  <ul>
+                    <li>понедельник – четверг: с 9-00 до 18-00;</li>
+                    <li>обеденный перерыв: с 13-00 до 13-45;</li>
+                    <li>пятница: с 9-00 до 16-20;</li>
+                    <li>обеденный перерыв: с 13-00 до 13-20;</li>
+                    <li>выходной: суббота, воскресенье.</li>
+                  </ul>
                 </div>
-                <ul>
-                  <li>понедельник – четверг: с 9-00 до 18-00;</li>
-                  <li>обеденный перерыв: с 13-00 до 13-45;</li>
-                  <li>пятница: с 9-00 до 16-20;</li>
-                  <li>обеденный перерыв: с 13-00 до 13-20;</li>
-                  <li>выходной: суббота, воскресенье.</li>
-                </ul>
-              </div>
-            </div>
+              </contact-note>
+            </contact>
           </div>
+
           <div class="col-lg-6 col-12 d-flex align-items-center">
             <yandex-map
               :settings="settings"
@@ -72,27 +74,30 @@
 
 <script>
 import { yandexMap, ymapMarker } from 'vue-yandex-maps';
+import ContactInfoList from '@/components/Contact/ContactInfoList.vue';
+import ContactNote from '@/components/Contact/ContactNote.vue';
 
 export default {
   name: 'Contacts',
   components: {
+    ContactInfoList,
+    ContactNote,
     yandexMap,
-    ymapMarker
+    ymapMarker,
   },
-  data() {
-    return {
-      settings: {
-        lang: 'ru_RU',
-        coordorder: 'latlong',
-        version: '2.1',
-      }
-    };
-  },
+  data: () => ({
+    settings: {
+      lang: 'ru_RU',
+      coordorder: 'latlong',
+      version: '2.1',
+    },
+  }),
 };
 </script>
 
 <style lang="stylus" scoped>
-.ymap-container
-  width: 100%
-  height: 400px
+.ymap-container {
+  width: 100%;
+  height: 400px;
+}
 </style>

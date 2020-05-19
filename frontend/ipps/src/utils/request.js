f667eada77a537529631bf7694df6529622b389d
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-19 14:58:02 +0300

    delete console log

--- frontend/ipps/src/utils/request.js
+++ frontend/ipps/src/utils/request.js
@@ -2,36 +2,30 @@ import axios from 'axios';
 import router from '@/router';
 
 const service = axios.create({
-  baseURL: 'http://www.ipps.by:5454/client-api/', // global server
-  // baseURL: 'http://192.168.1.125:8080/client-api/', // local server
-  timeout: 5000
+  baseURL: 'http://www.ipps.by:5454/client-api/',
+  timeout: 5000,
 });
 
 service.interceptors.request.use(
   config => config,
   error => {
-    // eslint-disable-next-line no-console
-    console.log(1, { error });
     return Promise.reject(error);
-  }
+  },
 );
 
 service.interceptors.response.use(
   response => {
-    // eslint-disable-next-line no-console
     const { data } = response;
     return data;
   },
   error => {
-    // eslint-disable-next-line no-console
-    console.log(2, { error });
     if (error.response.status === 404) {
       router.push({ name: '404' });
     } else if (typeof error.response === 'undefined') {
       router.push({ name: 'network-issue' });
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default service;

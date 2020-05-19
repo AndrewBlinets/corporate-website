ca589ff8841206c43a482993af0fa2ea741cf863
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-19 14:55:14 +0300

    update getters mobule

--- frontend/ipps/src/store/getters.js
+++ frontend/ipps/src/store/getters.js
@@ -1,9 +1,7 @@
 const getters = {
-  partners: state => state.main.partnersList,
-  contacts: state => state.contacts.contactsList,
   news: state => state.news.newsList,
   article: state => state.news.article,
   projects: state => state.project.projectsList,
-  project: state => state.project.project
+  project: state => state.project.project,
 };
 export default getters;

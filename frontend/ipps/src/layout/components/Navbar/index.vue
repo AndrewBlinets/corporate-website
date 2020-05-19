ec7e868d568e43d836affcc11b8e724792bc248f
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-19 14:53:18 +0300

    add new menu in navbar

--- frontend/ipps/src/layout/components/Navbar/index.vue
+++ frontend/ipps/src/layout/components/Navbar/index.vue
@@ -7,47 +7,55 @@
     <div class="app-container">
       <div class="content">
         <div class="logo">
-          <img src="@/images/logoIPPS.png" alt="ИППС" srcset="">
+          <app-image :id="142" name="ИППС" backgroundColor="#ffffff00" />
         </div>
-        <app-menu />
+
+        <button-menu
+          v-if="mobile"
+          :dark="!menuShrink"
+          @click="openSidebar"
+        ></button-menu>
+        <app-menu v-else :dark="menuShrink" />
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
 import { Scroll } from '@/directive/scroll';
-import AppMenu from './Menu';
+import AppMenu from './components/Menu.vue';
+import AppImage from '@/components/AppImage/index.vue';
+import ButtonMenu from './components/ButtonMenu.vue';
 
 export default {
   name: 'Navbar',
   components: {
-    AppMenu
+    AppMenu,
+    AppImage,
+    ButtonMenu,
   },
   directives: {
-    Scroll
+    Scroll,
   },
-  data() {
-    return {
-      menuShrink: false,
-      boxNext: false,
-      sections: ['company'],
-      dimensions: {
-        products: { width: 490, height: 280, x: 0 },
-        developers: { width: 390, height: 266, x: 100 },
-        company: { width: 260, height: 296, x: 200 }
-      }
-    };
-  },
-  created() {
-    setInterval(() => {
-      this.boxNext = !this.boxNext;
-    }, 2000);
+  data: () => ({
+    menuShrink: false,
+  }),
+  computed: {
+    ...mapState('app', ['mobile', 'sidebar']),
   },
   methods: {
+    ...mapActions('app', ['openSidebar']),
     handleSroll() {
       this.menuShrink = window.scrollY > 50;
-    }
-  }
+    },
+  },
 };
 </script>
+
+<style lang="stylus" scoped>
+.logo {
+  width: 50px;
+  height: 50px;
+}
+</style>

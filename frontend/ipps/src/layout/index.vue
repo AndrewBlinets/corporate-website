92756db1a7b6366b4727692175db081eba0a323d
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-19 14:52:32 +0300

    add sidebar in layout

--- frontend/ipps/src/layout/index.vue
+++ frontend/ipps/src/layout/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app-wraper">
     <navbar />
+    <sidebar />
     <app-main />
     <app-footer />
   </div>
@@ -10,24 +11,29 @@
 import AppMain from './components/AppMain';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar/index';
+import Sidebar from './components/Sidebar';
 
 export default {
   name: 'Layout',
   components: {
     AppMain,
     Navbar,
-    AppFooter: Footer
-  }
+    Sidebar,
+    AppFooter: Footer,
+  },
 };
 </script>
 
 <style lang="stylus" scoped>
-.app-wraper
-  display flex
-  flex-direction column
-  min-height: 100vh
-  .header
-    width 100%
-    height 80px
-    background #000  
+.app-wraper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  .header {
+    width: 100%;
+    height: 80px;
+    background: #000;
+  }
+}
 </style>

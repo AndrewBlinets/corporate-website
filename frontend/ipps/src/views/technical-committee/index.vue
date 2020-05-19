03846d56548eb5aa7c90310b53f95e83d5de6686
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-19 15:18:04 +0300

    add request page api in technical-committee

--- frontend/ipps/src/views/technical-committee/index.vue
+++ frontend/ipps/src/views/technical-committee/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    <header-page :image="page.imageId">
+    <header-page :image="20">
       <h1>{{ page.name }}</h1>
     </header-page>
     <div class="body-page">
-      <section-component :section="section" />
+      <section-component
+        v-for="section in page.sections"
+        :key="section.id"
+        :page="page.id"
+        :section="section"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import store from '@/store';
+import { mapState } from 'vuex';
 import HeaderPage from '@/components/HeaderPage';
 import SectionComponent from '@/components/Section';
 
@@ -17,25 +24,24 @@ export default {
   name: 'TechnicalCommittee',
   components: {
     HeaderPage,
-    SectionComponent
+    SectionComponent,
   },
-  data() {
-    return {
-      page: {
-        name: 'Национальный Технический Комитет',
-        imageId: 20
-      },
-      section: {
-        name: 'Новости',
-        type: 2
-      }
-    };
-  }
+  computed: {
+    ...mapState({
+      page: state => state.page.page,
+    }),
+  },
+  beforeRouteEnter(to, from, next) {
+    store.dispatch('page/getPage', 3).then(() => {
+      next();
+    });
+  },
 };
 </script>
 
 <style lang="stylus" scoped>
-.body-page
-  margin-top: 100px
-  margin-bottom: 100px
+.body-page {
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
 </style>

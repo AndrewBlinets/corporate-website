898fb478334ea3301f7a210a0a334d5f536e3713
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-19 14:57:45 +0300

    change activities component

--- frontend/ipps/src/views/home/components/Activities.vue
+++ frontend/ipps/src/views/home/components/Activities.vue
@@ -6,14 +6,17 @@
       </div>
       <div class="section-body">
         <div class="services-container">
+          <div class="services-img">
+            <app-image :id="143" name="направления-деятельности"></app-image>
+          </div>
           <div class="row no-gutters">
             <div
-              v-for="servise in servises"
-              :key="servise.id"
+              v-for="(activity, index) in activities"
+              :key="index"
               class="col"
             >
               <div class="service-item">
-                <h5>{{ servise.title }}</h5>
+                <h5>{{ activity }}</h5>
               </div>
             </div>
           </div>
@@ -24,88 +27,87 @@
 </template>
 
 <script>
+import AppImage from '@/components/AppImage';
+
 export default {
   name: 'Activities',
-  data() {
-    return {
-      servises: [{
-          id: 1,
-          title: 'Программный инжиниринг'
-        }, {
-          id: 2,
-          title: 'Создание инфраструктуры электронного правительства'
-        }, {
-          id: 3,
-          title: 'Развитие государственных и комерерческих электронных услуг'
-        }, {
-          id: 4,
-          title: 'Деятельность по технической защите информациии'
-        }, {
-          id: 5,
-          title: 'Сопровождение ранее разработанных информационных систем'
-        }, {
-          id: 6,
-          title: 'Государстенная регистрация информационных систем и информационных ресурсов'
-        }, {
-          id: 7,
-          title: 'Разработка информационных систем расчёта и анализа показателей состояния информатизации в Республике Беларусь'
-        }, {
-          id: 8,
-          title: 'Аутсорсинг государственных информационных систем'
-        }, {
-          id: 9,
-          title: 'Создание программного обеспечения для систем организационно-экономическог управления'
-        }, {
-          id: 10,
-          title: 'Комплексная автоматизация бухгалтерского учёта бюджетной сферы'
-        }, {
-          id: 11,
-          title: 'Координация проектов по эл. торговле, эл. логистике в восточном портнёрстве и цифровых транспортных коридорах'
-        }, {
-          id: 12,
-          title: 'Стандартизация в сфере цифровой трансформации'
-        }
-      ]
-    };
-  }
+  components: {
+    AppImage,
+  },
+  data: () => ({
+    activities: [
+      'Анализ, проектирование и программное обеспечение информационных систем',
+      'Обработка данных с применением ПО третьего лица или собственного ПО',
+      'Фундаментальные и прикладные исследования, экспериментальные разработки в области естественных и технических наук',
+      'Техническая и (или) криптографическая защита информации',
+      'Анализ информационных потребностей юридических лиц и индивидуальных предпринимателей',
+      'Консультирование по вопросам использования информационных технологий в целях инновации бизнес-процессов',
+      'Оказание автоматизированных услуг по поиску, отбору, обработке и сортировке данных',
+      'Услуги центров обработки данных',
+    ],
+  }),
 };
 </script>
 
 <style lang="stylus" scoped>
-.services-container
-  display: flex
-  flex-wrap: wrap
-  position: relative
-  background-image: url(../../../images/activities.jpg)
-  background-size: cover
-  background-position: center center
-  z-index: 0
-  &:before
-    content: ""
-    position: absolute
-    right: 0
-    bottom: 0
-    width: 100%
-    height: 100%
-    z-index: -1;
-    background: -webkit-linear-gradient(-45deg,rgba(88, 195, 255,.8) 0,rgba(0, 77, 230,.8) 40%,rgba(0, 59, 177,.8) 100%)
+.services-container {
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  overflow: hidden;
+  z-index: 0;
 
-.service-item
-  display: flex
-  min-width: 310px
-  height: 200px
-  padding: 20px
-  align-items: center
-  transition: all 0.4s ease
-  border-right: 2px solid #ffffff9e
-  border-bottom: 2px solid #ffffff9e
-  h5
-    color: #fff
-    transition: all 0.4s ease
-  &:hover
-    background-color: #1461FF
-    border-right-color: #fff
-    border-bottom-color: #fff
-    h5
-      transform: translateY(-7px)
+  &:before {
+    content: '';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    background: -webkit-linear-gradient(-45deg, rgba(88, 195, 255, 0.8) 0, rgba(0, 77, 230, 0.8) 40%, rgba(0, 59, 177, 0.8) 100%);
+    z-index: -1;
+  }
+
+  .services-img {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: -2;
+  }
+}
+
+.service-item {
+  display: flex;
+  min-width: 310px;
+  height: 250px;
+  padding: 20px;
+  align-items: center;
+  transition: all 0.4s cubic-bezier(0.4, 0, 0.6, 1);
+  border-right: 2px solid #ffffff9e;
+  border-bottom: 2px solid #ffffff9e;
+
+  h5 {
+    color: #fff;
+    transition: all 0.4s cubic-bezier(0.4, 0, 0.6, 1);
+  }
+
+  &:hover {
+    background-color: #1461FF;
+    border-right-color: #fff;
+    border-bottom-color: #fff;
+
+    h5 {
+      transform: translateY(-7px);
+    }
+  }
+}
+
+@media (max-width: 650px) {
+  .service-item {
+    height: 100px;
+  }
+}
 </style>

0a5346a10fcdcb129c89ea8686faa36480abd335
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-19 14:24:20 +0300

    add detect mobile device

--- frontend/ipps/src/App.vue
+++ frontend/ipps/src/App.vue
@@ -5,7 +5,24 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
-  name: 'App'
+  name: 'App',
+  computed: {
+    isMobile() {
+      return (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent,
+        ) || window.innerWidth < 1178
+      );
+    },
+  },
+  created() {
+    this.hasMobileDevice(this.isMobile);
+  },
+  methods: {
+    ...mapActions('app', ['hasMobileDevice']),
+  },
 };
 </script>

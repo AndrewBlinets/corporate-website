0732aa94ae7cfd4b2645919c23cc7acb4d77af26
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-22 18:26:57 +0300

    delete link of google drive in sidebar

--- frontend/admin-ipps/src/layout/components/Sidebar/index.vue
+++ frontend/admin-ipps/src/layout/components/Sidebar/index.vue
@@ -12,10 +12,6 @@
       :item="route"
       :base-path="route.path"
     />
-    <el-menu-item index="5">
-      <i class="fab fa-google-drive icon"></i>
-      <el-link class="link-menu" target="_blank" href="https://docs.google.com/presentation/d/1WNnmVUKoe8WZ2rTh1xuwkjghVYVDRh_StFHiSVSkPr0/edit?usp=sharing">Google Drive</el-link>
-    </el-menu-item>    
   </el-menu>
 </template>
 
@@ -26,28 +22,29 @@ import SidebarItem from './SidebarItem';
 export default {
   name: 'Sidebar',
   components: {
-    SidebarItem
+    SidebarItem,
   },
   computed: {
-    ...mapGetters([
-      'permissionRoutes'
-    ])
-  }
+    ...mapGetters(['permissionRoutes']),
+  },
 };
 </script>
 
 <style lang="stylus" scoped>
-.el-menu
-  height: 100%
+.el-menu {
+  height: 100%;
+}
 
-.icon
-  margin-right: 5px
-  width: 24px
-  text-align: center
-  font-size: 18px
-  vertical-align: middle
-  color: #909399
+.icon {
+  margin-right: 5px;
+  width: 24px;
+  text-align: center;
+  font-size: 18px;
+  vertical-align: middle;
+  color: #909399;
+}
 
-.link-menu
-  color: rgb(191, 203, 217)
+.link-menu {
+  color: rgb(191, 203, 217);
+}
 </style>

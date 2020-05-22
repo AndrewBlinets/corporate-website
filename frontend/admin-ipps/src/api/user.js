8a7401173b5d80e98ce88d90473e261836ca560d
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-22 18:27:48 +0300

    add link logout method in user api

--- frontend/admin-ipps/src/api/user.js
+++ frontend/admin-ipps/src/api/user.js
@@ -3,21 +3,21 @@ import request from '@/utils/request';
 export function login(data) {
   return request({
     url: '/authenticate',
-    method: 'post',
-    data
+    method: 'POST',
+    data,
   });
 }
 
 export function logout() {
   return request({
-    url: '/logout',
-    method: 'post'
+    url: '/logoutUser',
+    method: 'GET',
   });
 }
 
 export function getInfo() {
   return request({
     url: '/getInfo',
-    method: 'get'
+    method: 'GET',
   });
 }

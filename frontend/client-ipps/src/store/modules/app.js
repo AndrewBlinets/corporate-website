40ca76612b04e7c573d4f6934828870e3ef7d60b
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-05-28 20:06:55 +0300

    add app sotre module

--- frontend/client-ipps/src/store/modules/app.js
+++ frontend/client-ipps/src/store/modules/app.js
@@ -1,8 +1,27 @@
-const state = {};
+const state = {
+  sidebar: true,
+};
 
-const mutations = {};
+const mutations = {
+  SET_SIDEBAR: (state, value) => {
+    state.sidebar = value;
+  },
+  TOGGLE_SIDEBAR: state => {
+    state.sidebar = !state.sidebar;
+  },
+};
 
-const actions = {};
+const actions = {
+  openSidebar({ commit }) {
+    commit('SET_SIDEBAR', true);
+  },
+  closeSidebar({ commit }) {
+    commit('SET_SIDEBAR', false);
+  },
+  toggleSidebar({ commit }) {
+    commit('TOGGLE_SIDEBAR');
+  },
+};
 
 export default {
   namespaced: true,

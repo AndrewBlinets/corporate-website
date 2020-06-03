164c2f99c94533edbe54e115079538a6820adf8a
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-06-03 22:49:32 +0300

    expanded user store module

--- frontend/admin-ipps/src/store/modules/user.js
+++ frontend/admin-ipps/src/store/modules/user.js
@@ -1,10 +1,13 @@
 import { login, logout, getInfo } from '@/api/user';
 import { getToken, setToken, removeToken } from '@/utils/auth';
+import { resetRouter } from '../../router';
 
 const state = {
   token: getToken(),
   name: '',
-  roles: []
+  surname: '',
+  patronic: '',
+  roles: [],
 };
 
 const mutations = {
@@ -14,65 +17,84 @@ const mutations = {
   SET_NAME: (state, name) => {
     state.name = name;
   },
+  SET_SURNAME: (state, surname) => {
+    state.surname = surname;
+  },
+  SET_PATRONYMIC: (state, patronic) => {
+    state.patronic = patronic;
+  },
   SET_ROLES: (state, roles) => {
     state.roles = roles;
-  }
+  },
 };
 
 const actions = {
-  login({ commit }, userInfo = { username: 'javainuse', password: 'password' }) {
+  login(
+    { commit },
+    userInfo = { username: 'javainuse', password: 'password' },
+  ) {
     const { username, password } = userInfo;
     return new Promise((resolve, reject) => {
-      login({ username, password }).then(data => {
-        commit('SET_TOKEN', data.token);
-        setToken(data.token);
-        resolve();
-      }).catch(error => {
-        reject(error);
-      });
+      login({ username, password })
+        .then(data => {
+          commit('SET_TOKEN', data.token);
+          setToken(data.token);
+          resolve();
+        })
+        .catch(error => {
+          reject(error);
+        });
     });
   },
 
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(data => {
-        const { name, roles } = data;
+      getInfo(state.token)
+        .then(data => {
+          const { name, surName, patronicName, roles } = data;
 
-        commit('SET_NAME', name);
-        commit('SET_ROLES', roles);
-        resolve(data);
-      }).catch(error => {
-        reject(error);
-      });
+          commit('SET_NAME', name);
+          commit('SET_SURNAME', surName);
+          commit('SET_PATRONYMIC', patronicName);
+          commit('SET_ROLES', roles);
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
     });
   },
 
-  logout({ commit, state }) {
+  logout({ dispatch }) {
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        commit('SET_TOKEN', '');
-        commit('SET_ROLES', []);
-        removeToken();
-        resolve();
-      }).catch(error => {
-        reject(error);
-      });
+      logout()
+        .then(() => {
+          dispatch('resetToken');
+          resetRouter();
+          resolve();
+        })
+        .catch(error => {
+          reject(error);
+        });
     });
   },
 
   resetToken({ commit }) {
     return new Promise(resolve => {
       commit('SET_TOKEN', '');
+      commit('SET_NAME', '');
+      commit('SET_SURNAME', '');
+      commit('SET_PATRONYMIC', '');
       commit('SET_ROLES', []);
       removeToken();
       resolve();
     });
-  }
+  },
 };
 
 export default {
   namespaced: true,
   state,
   mutations,
-  actions
+  actions,
 };

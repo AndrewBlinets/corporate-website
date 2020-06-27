96e8a00ef3d711a461f9cb32b89d924e03d4cdbc
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-06-27 14:08:12 +0300

    removed an unused imports

--- frontend/ipps/src/main.js
+++ frontend/ipps/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 
+import '@/styles/index.styl';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
 import VueYouTubeEmbed from 'vue-youtube-embed';
 
 import App from './App.vue';
@@ -8,37 +11,7 @@ import router from '@/router';
 import store from '@/store';
 import * as filters from '@/filters';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faCommentAlt,
-  faUserTie,
-  faBriefcase,
-  faClock,
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  faFacebookF,
-  faVk,
-  faTwitter,
-  faYoutube,
-} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-import '@/styles/index.styl';
-import 'bootstrap/dist/css/bootstrap-grid.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import '@fortawesome/fontawesome-free/js/all.js';
-
-library.add(
-  faCommentAlt,
-  faFacebookF,
-  faVk,
-  faTwitter,
-  faYoutube,
-  faUserTie,
-  faBriefcase,
-  faClock,
-);
-
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;

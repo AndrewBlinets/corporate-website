8595d63389111106dfa162a6432d2d325c4f80ff
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-07-01 09:51:22 +0300

    expanded VButton component

--- frontend/ipps/src/components/UI/VButton.vue
+++ frontend/ipps/src/components/UI/VButton.vue
@@ -1,7 +1,6 @@
 <template>
   <button
-    class="v-btn v-size--default"
-    :class="[fontSizeClass]"
+    :class="[classes]"
     :style="{ color: color, backgroundColor: color }"
     v-bind="$attrs"
     v-on="$listeners"
@@ -16,15 +15,37 @@
 export default {
   name: 'VButton',
   props: {
+    color: String,
+    dark: Boolean,
     fontSize: {
       type: String,
       default: 'default',
     },
-    color: String,
+    tag: {
+      type: String,
+      default: 'button',
+    },
+    text: Boolean,
+    to: {
+      type: Object,
+      default: () => ({}),
+    },
   },
   computed: {
-    fontSizeClass() {
-      return `v-size--${this.fontSize}`;
+    classes() {
+      return {
+        'v-btn': true,
+        [`v-size--${this.fontSize}`]: true,
+        'v-btn--contained': this.contained,
+        'v-btn--text': this.text,
+        'theme--dark': this.dark,
+      };
+    },
+    contained() {
+      return !this.isFlat;
+    },
+    isFlat() {
+      return this.text;
     },
   },
 };
@@ -39,9 +60,9 @@ export default {
   border-radius: 4px;
   border-style: none;
   font-size: 16px;
-  text-transform: uppercase;
+  text-transform: capitalize;
   cursor: pointer;
-  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+  color: inherit;
 
   &::before {
     position: absolute;
@@ -61,20 +82,34 @@ export default {
     opacity: 0.08;
   }
 
+  &.v-btn--text {
+    background-color: transparent;
+  }
+
+  &.v-btn--contained {
+    box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+  }
+
+  &.theme--dark {
+    color: #fff;
+  }
+
   .v-btn__content {
+    display: flex;
+    align-items: center;
     color: inherit;
   }
 
   &.v-size--default {
-    font-size: 0.875rem;
-  }
-
-  &.v-size--medium {
     font-size: 1rem;
   }
 
-  &.v-size--large {
+  &.v-size--medium {
     font-size: 1.125rem;
+  }
+
+  &.v-size--large {
+    font-size: 1.25rem;
   }
 }
 </style>

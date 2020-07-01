1d8c57a4d30738cad543d8541692713eba01b1d5
Alexei Zubarik <alexei.zubarik@ynadex.ru>
2020-07-01 09:53:30 +0300

    add VMenu component in menu

--- frontend/ipps/src/layout/components/Navbar/components/Menu.vue
+++ frontend/ipps/src/layout/components/Navbar/components/Menu.vue
@@ -13,6 +13,31 @@
       >
         {{ link.name }}
       </router-link>
+
+      <v-menu>
+        <template v-slot:activator="{ on, attrs }">
+          <v-button text v-bind="attrs" v-on="on">
+            Ещё
+            <font-awesome-icon :icon="caretDownIcon" class="icon-right" />
+          </v-button>
+        </template>
+
+        <div class="list--nav">
+          <router-link
+            :to="{ name: 'eastern-partnership' }"
+            class="sub-nav--link"
+          >
+            Международное Сотрудничество
+          </router-link>
+
+          <router-link
+            :to="{ name: 'technical-committee' }"
+            class="sub-nav--link"
+          >
+            Национальный Технический Комитет
+          </router-link>
+        </div>
+      </v-menu>
     </div>
   </div>
 </template>
@@ -20,9 +45,16 @@
 <script>
 import { Resize } from '@/directive/resize';
 import { mapState, mapActions } from 'vuex';
+import VButton from '@/components/UI/VButton';
+import VMenu from '@/components/UI/VMenu';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 export default {
   name: 'Menu',
+  components: {
+    VButton,
+    VMenu,
+  },
   directives: {
     Resize,
   },
@@ -54,19 +86,13 @@ export default {
         name: 'Контакты',
         path: 'contacts',
       },
-
-      {
-        name: 'Восточное Партнерство',
-        path: 'eastern-partnership',
-      },
-      {
-        name: 'Национальный Технический Комитет',
-        path: 'technical-committee',
-      },
     ],
   }),
   computed: {
     ...mapState('app', ['mobile', 'sidebar']),
+    caretDownIcon() {
+      return faCaretDown;
+    },
   },
   watch: {
     $route: 'closeSidebar',
@@ -91,6 +117,7 @@ export default {
     .menu {
       flex-direction: column;
       align-items: flex-start;
+      color: #fff;
 
       .menu__link {
         justify-content: flex-start;
@@ -103,6 +130,12 @@ export default {
 
 .menu {
   display: flex;
+  themify(
+    $themes,
+    @($theme) {
+    color: $theme.$navbar--menu-link--color;
+  }
+  );
 
   .menu__link {
     position: relative;
@@ -110,11 +143,18 @@ export default {
     align-items: center;
     justify-content: center;
     font-weight: 400;
-    color: #fff;
+    font-size: 16px;
     min-width: 48px;
     min-height: 36px;
     padding: 4px 16px;
     border-radius: 4px;
+    margin-right: 5px;
+    themify(
+      $themes,
+      @($theme) {
+      color: $theme.$navbar--menu-link--color;
+    }
+    );
 
     &::before {
       content: '';
@@ -135,9 +175,67 @@ export default {
   }
 
   &.dark-menu {
-    .menu__link {
-      color: #222;
+    themify(
+      $themes,
+      @($theme) {
+      color: $theme.$navbar--menu-link--dark--color;
     }
+    );
+
+    .menu__link {
+      themify(
+        $themes,
+        @($theme) {
+        color: $theme.$navbar--menu-link--dark--color;
+      }
+      );
+    }
+  }
+}
+
+.icon-right {
+  margin-left: 8px;
+  font-size: 12px;
+}
+
+.list--nav {
+  display: flex;
+  flex-direction: column;
+  padding: 0 8px;
+
+  .sub-nav--link {
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding: 0 8px;
+    min-height: 40px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1rem;
+    white-space: nowrap;
+    color: inherit;
+    border-radius: 4px;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      border-radius: inherit;
+      opacity: 0;
+      transition: opacity 0.2s cubic-bezier(0.4, 0, 0.6, 1);
+      background-color: currentColor;
+    }
+
+    &:hover::before {
+      opacity: 0.08;
+    }
+  }
+
+  .sub-nav--link:not(:last-child) {
+    margin-bottom: 4px;
   }
 }
 </style>

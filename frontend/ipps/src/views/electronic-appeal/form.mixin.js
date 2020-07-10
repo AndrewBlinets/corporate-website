b70a58cd834214a1a4c1eebfadda471d5182c58e
Alexei Zubarik <alesha.zubarik@yandex.ru>
2020-07-10 11:10:46 +0300

    add new data form, email validation, complete send

--- frontend/ipps/src/views/electronic-appeal/form.mixin.js
+++ frontend/ipps/src/views/electronic-appeal/form.mixin.js
@@ -5,7 +5,13 @@ import VTextareas from '@/components/UI/VTextareas';
 import VFileInput from '@/components/UI/VFileInput';
 
 import { validationMixin } from 'vuelidate';
-import { required, maxLength } from 'vuelidate/lib/validators';
+import {
+  required,
+  requiredIf,
+  maxLength,
+  email,
+} from 'vuelidate/lib/validators';
+import { Object } from 'core-js';
 
 export const formMixin = {
   components: {
@@ -16,55 +22,71 @@ export const formMixin = {
 
   mixins: [validationMixin],
   validations: {
-    jurName: { required },
-    postcode: { required },
-    name: { required },
-    address: { required },
-    email: { required },
-    textAppeal: { required, maxLength: maxLength(2000) },
+    form: {
+      jurName: {
+        required: requiredIf(function() {
+          return this.form.type === 2;
+        }),
+      },
+      postcode: {
+        required: requiredIf(function() {
+          return this.form.type === 2;
+        }),
+      },
+      name: { required },
+      address: { required },
+      email: { required, email },
+      textAppeal: { required, maxLength: maxLength(2000) },
+    },
   },
 
   data: () => ({
-    name: '',
-    address: '',
-    email: '',
-    textAppeal: '',
-    file: null,
+    form: {
+      name: '',
+      address: '',
+      email: '',
+      textAppeal: '',
+      file: null,
+    },
+    sendStatus: null,
   }),
 
   computed: {
     nameErrors() {
       const errors = [];
 
-      if (!this.$v.name.$dirty) return errors;
-      !this.$v.name.required && errors.push('Поле обязательно');
+      if (!this.$v.form.name.$dirty) return errors;
+      !this.$v.form.name.required && errors.push('Поле обязательно');
 
       return errors;
     },
     addressErrors() {
       const errors = [];
 
-      if (!this.$v.address.$dirty) return errors;
-      !this.$v.address.required && errors.push('Поле обязательно');
+      if (!this.$v.form.address.$dirty) return errors;
+      !this.$v.form.address.required && errors.push('Поле обязательно');
 
       return errors;
     },
     emailErrors() {
       const errors = [];
 
-      if (!this.$v.email.$dirty) return errors;
-      !this.$v.email.required && errors.push('Поле обязательно');
+      if (!this.$v.form.email.$dirty) return errors;
+      !this.$v.form.email.required && errors.push('Поле обязательно');
+      !this.$v.form.email.email &&
+        !this.$refs.email.hasFocus &&
+        errors.push('Некорректный электронный адрес');
 
       return errors;
     },
     textAppealErrors() {
       const errors = [];
 
-      if (!this.$v.textAppeal.$dirty) return errors;
-      !this.$v.textAppeal.required && errors.push('Поле обязательно');
-      !this.$v.textAppeal.maxLength &&
+      if (!this.$v.form.textAppeal.$dirty) return errors;
+      !this.$v.form.textAppeal.required && errors.push('Поле обязательно');
+      !this.$v.form.textAppeal.maxLength &&
         errors.push(
-          `Максимум ${this.$v.textAppeal.$params.maxLength.max} символов`,
+          `Максимум ${this.$v.form.textAppeal.$params.maxLength.max} символов`,
         );
 
       return errors;
@@ -73,16 +95,29 @@ export const formMixin = {
 
   methods: {
     send() {
-      let formData = new FormData();
+      this.$v.form.$touch();
 
-      for (const [key, value] of Object.entries(this.$data)) {
-        formData.append(key, value);
+      if (!this.$v.form.$error && this.sendStatus !== 'PENDING') {
+        this.sendStatus = 'PENDING';
+        let formData = new FormData();
+
+        for (const [key, value] of Object.entries(this.form)) {
+          value && formData.append(key, value);
+        }
+
+        sendAppeal(formData)
+          .then(() => {
+            this.sendStatus = 'OK';
+
+            Object.keys(this.form).forEach(key => {
+              if (!['file', 'type'].includes(key)) this.form[key] = '';
+            });
+            this.$v.form.$reset();
+          })
+          .catch(() => {
+            this.sendStatus = 'ERROR';
+          });
       }
-
-      sendAppeal(formData).then(() => {
-        // eslint-disable-next-line no-console
-        console.log('OK');
-      });
     },
   },
 };
